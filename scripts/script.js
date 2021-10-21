96366ecce95f0ef2d994ce75eb4ada106658dddc
Leonardo Rodriguez <leonardo.rodriguez9176@gmail.com>
2021-10-21 13:08:51 -0500

    text area added to inser names

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,3 @@
-function randomSelector(studentsList) {
-    return Math.round(Math.random() * (studentsList.length - 1));
-}
-
 function createGroups(studentsList) {
     const studentsAux = [...studentsList];
 
@@ -9,10 +5,10 @@ function createGroups(studentsList) {
     const maxGroupSize = 2;
 
     while (studentsAux.length) {
-        let groupAux = [];
+        const groupAux = [];
         for (let i = 0; i < maxGroupSize; i++) {
             if (studentsAux.length) {
-                const studentSelected = randomSelector(studentsAux);
+                const studentSelected = Math.round(Math.random() * (studentsAux.length - 1));
                 groupAux.push(studentsAux[studentSelected]);
                 studentsAux.splice(studentSelected, 1);
             }
@@ -22,36 +18,42 @@ function createGroups(studentsList) {
     return groups;
 }
 
-let students = [];
-students.push('ANDRES FELIPE GIRALDO');
-students.push('EDWIN GERMAN VILLALBA GONZALEZ');
-students.push('CRISTIAN ORLANDO ROMERO ACOSTA');
-students.push('CRISTIAN DAVID SAINEA CÉSPEDES');
-students.push('DANIEL CAMILO RAMIREZ MARTINEZ');
-students.push('ALEJANDRO RIVAS LOPEZ');
-students.push('DIANA GARCIA SANDOVAL');
-students.push('MARCOS ORTIZ');
-students.push('CARLOS ANDRES FANDIÑO RUEDA');
-students.push('JORGE ALBERTO RODRIGUEZ PAEZ');
-students.push('EDGAR LEONARDO RODRIGUEZ VASQUEZ');
-students.push('KELLY JOHANA VERGARA TORRES');
-students.push('LINA MARÍA RODRÍGUEZ RAMÍREZ');
-students.push('JOHN JAIRO ORJUELA ESPINOSA');
-students.push('JHON FAVER MACHADO ALAPE');
-students.push('FRANCISCO JAVIER TORRES VILLAMOR');
-let groups = createGroups(students);
+let textAreaList = '';
+textAreaList += 'ANDRES FELIPE GIRALDO\n';
+textAreaList += 'EDWIN GERMAN VILLALBA GONZALEZ\n';
+textAreaList += 'CRISTIAN ORLANDO ROMERO ACOSTA\n';
+textAreaList += 'CRISTIAN DAVID SAINEA CÉSPEDES\n';
+textAreaList += 'DANIEL CAMILO RAMIREZ MARTINEZ\n';
+textAreaList += 'ALEJANDRO RIVAS LOPEZ\n';
+textAreaList += 'DIANA GARCIA SANDOVAL\n';
+textAreaList += 'MARCOS ORTIZ\n';
+textAreaList += 'CARLOS ANDRES FANDIÑO RUEDA\n';
+textAreaList += 'JORGE ALBERTO RODRIGUEZ PAEZ\n';
+textAreaList += 'EDGAR LEONARDO RODRIGUEZ VASQUEZ\n';
+textAreaList += 'KELLY JOHANA VERGARA TORRES\n';
+textAreaList += 'LINA MARÍA RODRÍGUEZ RAMÍREZ\n';
+textAreaList += 'JOHN JAIRO ORJUELA ESPINOSA\n';
+textAreaList += 'JHON FAVER MACHADO ALAPE\n';
+textAreaList += 'FRANCISCO JAVIER TORRES VILLAMOR\n';
+
+const studentsList = document.getElementById('students-text-box');
+studentsList.innerHTML = textAreaList.trim();
+
 
 //DOM
 const h1 = document.createElement('h1');
 h1.innerText = `Creación aleatoria de grupos`;
 document.body.appendChild(h1);
+const divTextBox = document.getElementById('div-text-box');
+divTextBox.before(h1);
+
 
 const bodyMain = document.createElement('main');
 bodyMain.setAttribute('id', 'body-main');
 document.body.appendChild(bodyMain);
 
 const divButton = document.createElement('div');
-divButton.setAttribute('id', 'div-button');
+divButton.setAttribute('class', 'div-button');
 bodyMain.appendChild(divButton);
 
 const button = document.createElement('button');
@@ -59,35 +61,48 @@ button.innerText = `Crear grupos`;
 button.setAttribute('id', 'create-groups-button');
 divButton.appendChild(button);
 
+//Eevents
+
+//Clear list
+document.getElementById("button-clean-list").onclick = () => {
+    const studentsList = document.getElementById('students-text-box');
+    studentsList.value = '';
+};
+
+//Create groups
 const buttonClick = document.querySelector('#create-groups-button');
 buttonClick.addEventListener('click', (eventFunctOpt1) => {
 
-    groups = createGroups(students);
+    const studentsList = document.getElementById('students-text-box');
+    const textAreaList = studentsList.value.trim();
+    const students = textAreaList.split('\n');
+
+    const groups = [...createGroups(students)];
+
     const checkDivGroup = document.getElementById('div-groups');
     if (document.body.contains(checkDivGroup)) {
-
         const father = checkDivGroup.parentElement;
         father.removeChild(checkDivGroup);
     }
     const divGroups = document.createElement('div');
-    divGroups.setAttribute('id', 'div-groups');
-    bodyMain.appendChild(divGroups);
+        divGroups.setAttribute('id', 'div-groups');
+        bodyMain.appendChild(divGroups);
 
-    for (let i = 0; i < groups.length; i++) {
+        for (let i = 0; i < groups.length; i++) {
 
-        const divGroupContainer = document.createElement('div');
-        divGroupContainer.setAttribute('class', 'div-group-container');
-        divGroups.appendChild(divGroupContainer);
+            const divGroupContainer = document.createElement('div');
+            divGroupContainer.setAttribute('class', 'div-group-container');
+            divGroups.appendChild(divGroupContainer);
 
-        const pGroup = document.createElement('p');
-        pGroup.setAttribute('class', 'p-group');
-        pGroup.innerText = `Grupo ${i + 1}`;
-        divGroupContainer.appendChild(pGroup);
-        for (let j = 0; j < groups[i].length; j++) {
-            const groupMember = document.createElement('p');
-            groupMember.setAttribute('class', 'p-group-member');
-            groupMember.innerText = `${groups[i][j].toLowerCase()}`;
-            divGroupContainer.appendChild(groupMember);
+            const pGroup = document.createElement('p');
+            pGroup.setAttribute('class', 'p-group');
+            pGroup.innerText = `Grupo ${i + 1}`;
+            divGroupContainer.appendChild(pGroup);
+            for (let j = 0; j < groups[i].length; j++) {
+                const groupMember = document.createElement('p');
+                groupMember.setAttribute('class', 'p-group-member');
+                groupMember.innerText = `${groups[i][j].toLowerCase()}`;
+                divGroupContainer.appendChild(groupMember);
+            }
         }
-    }
 });
